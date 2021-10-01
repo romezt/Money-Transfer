2aed2f4b04b5a08bf5136b94baccf00ebaa541c8
Anton <ikkii780@mail.ru>
2021-10-01 12:38:37 +0500

    java1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int transition = 500_000_000;
+        int total = current + transition;
+        System.out.println(total);
+    }
+}
